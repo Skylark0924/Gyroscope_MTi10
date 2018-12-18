a2a3870fb263bec71200cb7c7d8e6cd4368b60e0
Skylark <Skylark0924@users.noreply.github.com>
2018-12-18 20:25:43 +0800

    delete some annotation

--- XsensPC1/Program.cs
+++ XsensPC1/Program.cs
@@ -25,59 +25,18 @@ namespace XsensPC1
         [DllImport("Dll1", EntryPoint = "mygetEulerdata", CharSet = CharSet.Ansi)]
         public static extern IntPtr mygetEulerdata();
 
-        [DllImport("Dll1.dll", EntryPoint = "CloseCOMDevice", CharSet = CharSet.Ansi)]
+        [DllImport("Dll1", EntryPoint = "CloseCOMDevice", CharSet = CharSet.Ansi)]
         public static extern void CloseCOMDevice();
 
         public static byte cResult = 1;
 
-        //[DllImport(@"../../../Release/Dll1.dll", EntryPoint = "test01", SetLastError = true, CharSet = CharSet.Ansi, ExactSpelling = false, CallingConvention = CallingConvention.StdCall)]
-        //extern static int test01(int a, int b, int c);
-
-        //[DllImport(@"../../../Release/Dll1.dll", EntryPoint = "test02", SetLastError = true, CharSet = CharSet.Ansi, ExactSpelling = false, CallingConvention = CallingConvention.StdCall)]
-        //extern static int test02(int a, int b);
         /// <summary>
         /// 应用程序的主入口点。
         /// </summary>
         [STAThread]
         static void Main(string[] args)
         {
-            //Application.EnableVisualStyles();
-            //Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
-            //int r1 = test01(1, 2, 3);
-            //int r2 = test02(5, 2);
-            //Console.WriteLine("test01结果：" + r1.ToString());
-            //Console.WriteLine("test02结果：" + r2.ToString());
-            //Console.Read();
-            //IntPtr pCaldata = Marshal.AllocHGlobal(128);
-            //Marshal.WriteByte(pCaldata, 0);
-            //mygetCaldata(pCaldata);
-            //string strRes = Marshal.PtrToStringAnsi(pCaldata);
-            //Console.WriteLine(strRes.ToString());
 
-            //Marshal.FreeHGlobal(pCaldata);
-
-            //IntPtr pRet = mygetCaldata();
-            //string strRet = Marshal.PtrToStringAnsi(pRet);
-            //Console.WriteLine("返回值：");
-            //Console.WriteLine(strRet);
-
-            //double[] ab = mygetCaldata();
-            //IntPtr inper = mygetCaldata();
-            //Console.WriteLine("返回值：");
-            //Console.WriteLine(inper);
-            //for (int j=0;j<10000; j++)
-            //{
-            //    IntPtr pointer = mygetCaldata();
-            //    double[] myCaldata = new double[3];
-            //    Marshal.Copy(pointer, myCaldata, 0, 3);
-            //    for (int i = 0; i < 3; i++)
-            //    {
-            //        Console.WriteLine(myCaldata[i] + "\n");
-            //    }
-
-            //}
-           
             cResult = OpenCOMDevice();
             if (cResult == 0)
             {
@@ -89,27 +48,6 @@ namespace XsensPC1
         }
         static void Print()
         {
-            //IntPtr pointer1 = new IntPtr();
-            //IntPtr pointer2 = new IntPtr();
-            //float[] myCaldata = { 0, 0, 0 };
-            //float[] myEulerdata = { 0, 0, 0 };
-            //while (true) {
-            //    pointer1 = mygetCaldata();
-            //    Marshal.Copy(pointer1, myCaldata, 0, 3);
-            //    pointer2 = mygetEulerdata();
-            //    Marshal.Copy(pointer2, myEulerdata, 0, 3);
-            //    for (int i = 0; i < 3; i++)
-            //    {
-            //        Console.Write(myCaldata[i]+"  ");
-            //    }
-            //    for (int i = 0; i < 3; i++)
-            //    {
-            //        Console.Write(myEulerdata[i]+"  ");
-            //    }
-            //    Console.WriteLine();
-            //    Thread.Sleep(10);
-            //}
-
             IntPtr pointer = new IntPtr();
             float[] mydata = { 0, 0, 0, 0, 0, 0 };
             while (true)
@@ -122,9 +60,7 @@ namespace XsensPC1
                 }
                 Console.WriteLine();
                 Thread.Sleep(10);
-            }
-             
+            }             
         }
-   
     }
 }
