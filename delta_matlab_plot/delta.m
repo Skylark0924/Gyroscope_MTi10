b97883c2f62df3d44cb6e961f7943828a14d4c29
Skylark <Skylark0924@users.noreply.github.com>
2018-12-21 12:46:34 +0800

    add matlab plot program
    plot program and dataset

---
+++ delta_matlab_plot/delta.m
@@ -0,0 +1,77 @@
+data1=xlsread('data_normal');
+data2=xlsread('data_3err_RULDRM'); % 错误数据
+Acc_x_normal=data1(271:470,1);
+Acc_y_normal=data1(271:470,2);
+Acc_z_normal=data1(271:470,3);
+Acc_x_error=data2(97:296,1);  % 只需要改区间即可
+Acc_y_error=data2(97:296,2);
+Acc_z_error=data2(97:296,3);
+t=1:200; % 选取200点
+%% 正常数据与故障数据分图对比
+figure
+set(gcf,'outerposition',get(0,'screensize'));
+subplot(3,2,1)
+plot(t,Acc_x_normal);
+title('Accx normal')
+xlabel('t');
+ylabel('Accx');
+subplot(3,2,3)
+plot(t,Acc_y_normal);
+title('Accy normal')
+xlabel('t');
+ylabel('Accy');
+subplot(3,2,5)
+plot(t,Acc_z_normal);
+title('Accz normal')
+xlabel('t');
+ylabel('Accz');
+
+subplot(3,2,2)
+plot(t,Acc_x_error);
+title('Accx error')
+xlabel('t');
+ylabel('Accx');
+subplot(3,2,4)
+plot(t,Acc_y_error);
+title('Accy error')
+xlabel('t');
+ylabel('Accy');
+subplot(3,2,6)
+plot(t,Acc_z_error);
+title('Accz error')
+xlabel('t');
+ylabel('Accz');
+
+%% 正常数据和故障数据同图对比
+figure 
+plot(t,Acc_x_normal);
+hold on 
+plot(t,Acc_x_error);
+title('Accx')
+legend('normal','error');
+figure 
+plot(t,Acc_y_normal);
+hold on 
+plot(t,Acc_y_error);
+title('Accy')
+legend('normal','error');
+figure 
+plot(t,Acc_z_normal);
+hold on 
+plot(t,Acc_z_error);
+title('Accz')
+legend('normal','error');
+%% 误差图
+figure 
+error_x=Acc_x_normal-Acc_x_error;
+error_y=Acc_y_normal-Acc_y_error;
+error_z=Acc_z_normal-Acc_z_error;
+subplot(3,1,1)
+plot(t,error_x)
+title('Diff X');
+subplot(3,1,2)
+plot(t,error_y)
+title('Diff Y');
+subplot(3,1,3)
+plot(t,error_z)
+title('Diff Z');
---
+++ delta_matlab_plot/readme.txt
@@ -0,0 +1,4 @@
+1. data_normal 是正常数据   			自271开始运动
+2. data_1err_RU 是右上角螺钉松动		自107开始运动
+3. data_2err_RULD 是右上和左下松动		自88开始运动
+4. data_3err_RULDRM 右上、左下和右中都松动	自97开始运动
